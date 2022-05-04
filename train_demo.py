fb2079e81553283b95ead481651c8f3af87ab38c
mashengkun <40291153+ShengkunMa@users.noreply.github.com>
2022-05-04 22:46:10 +0800

    Update train_demo.py

--- train_demo.py
+++ train_demo.py
@@ -7,7 +7,6 @@ from data_kit import extractor_feature
 from extractor import FeatureExtractor
 from framework import train
 
-
 torch.cuda.set_device(3)
 
 def main():
@@ -62,34 +61,28 @@ def main():
     torch.cuda.manual_seed_all(seed)
 
     base_feature_path = 'feature/base_faeture.json'
-    novel_feature_path = 'feature/da_feature.json'
+    novel_feature_path = 'feature/novel_feature.json'
     
-    # t1 = time.time()
-    # model = FeatureExtractor(ckpt_path)
-    # model.cuda()
-    # extractor_feature(base_data_path, model, base_feature_path)
-    # extractor_feature(novel_data_path, model, novel_feature_path)
-    # t2 = time.time()
-    # print('End of feature extraction!')
-    # print('time: {:.2f}'.format(t2 - t1))
+    t1 = time.time()
+    model = FeatureExtractor(ckpt_path)
+    model.cuda()
+    extractor_feature(base_data_path, model, base_feature_path)
+    extractor_feature(novel_data_path, model, novel_feature_path)
+    t2 = time.time()
+    print('End of feature extraction!')
+    print('time: {:.2f}'.format(t2 - t1))
 
-
-
-
-    p=0.5
-    N = 10
-    for K in [1, 5]:
-        t1 = time.time()
-        print(f'starting {N}-way {K}-shot...')
-        acc_mean = train(base_feature_path=base_feature_path, novel_feature_path=novel_feature_path,
-                    N=N, K=K, Q=Q, num_task=num_task, sampled_num=sampled_num, k=k, alpha=alpha, p=p,
-                    classifiers=classifier ) 
-        t2 = time.time()
-        print('#################END################')
-        print(f'end {N}-way {K}-shot')
-        print(f'number of task: {num_task}')
-        print('acc_mean: {:.4f}'.format(acc_mean))
-        print('total time: {:.2f}'.format(t2 - t1))    
+    t1 = time.time()
+    print(f'starting {N}-way {K}-shot...')
+    acc_mean = train(base_feature_path=base_feature_path, novel_feature_path=novel_feature_path,
+                N=N, K=K, Q=Q, num_task=num_task, sampled_num=sampled_num, k=k, alpha=alpha, p=p,
+                classifiers=classifier ) 
+    t2 = time.time()
+    print('#################END################')
+    print(f'{N}-way {K}-shot')
+    print(f'number of task: {num_task}')
+    print('acc_mean: {:.4f}'.format(acc_mean))
+    print('total time: {:.2f}'.format(t2 - t1))    
 
 
 if __name__ == '__main__':
